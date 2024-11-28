01e1c981484f7f79fbcade069c7e1ac8ba5a1b07
SahajArora <sahajarora1906@gmail.com>
2024-11-28 22:41:46 +0530

    added antd table component

--- src/components/StudentTable.jsx
+++ src/components/StudentTable.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
+import { Table } from "antd";
 import fetchdata from "../utils/fetchdata";
 import Pagination from "./Pagination";
-import TableBody from "./TableBody";
+// import TableBody from "./TableBody";
 
 function StudentTable(props) {
   const { activeTab, openModal } = props;
@@ -24,25 +25,83 @@ function StudentTable(props) {
   let slicedStudentData = userDetails.slice(startingIndex, endingIndex);
 
   function prevbtnFunc() {
-    console.log("prev btn");
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
     }
   }
 
   function nextBtnFunc() {
-    console.log("next btn");
     if (currentPage < Math.ceil(userDetails.length / detailsPerPage)) {
       setCurrentPage(currentPage + 1);
     }
   }
+
+  /*
+  Ant Design Table Component Starting
+  */
+
+   // Ant Design table columns
+   const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (_, user) => (
+        <span
+          style={{ cursor: "pointer", color: "blue" }}
+          onClick={() => fetchUserData(user.id)}
+        >
+          <img
+            src={user.avatar}
+            alt={user.first_name}
+            style={{ width: "24px", marginRight: "5px" }}
+          />
+          {user.first_name} {user.last_name}
+        </span>
+      ),
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Phone No.",
+      dataIndex: "Phone",
+      key: "Phone",
+    },
+    {
+      title: "Email ID",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+    },
+    {
+      title: "Date Of Birth",
+      dataIndex: "date_of_birth",
+      key: "date_of_birth",
+    },
+  ];
+
+  const fetchUserData = async (userID) => {
+    const data = await fetchdata(`users/${userID}`);
+    openModal(data);
+  };
+  
+  /*
+  Ant Design Table Component Ending
+  */
 
   return (
     <div className="tables">
       {activeTab === "classes" && <div id="classes">This is classes div</div>}
       {activeTab === "student" && (
         <div id="student">
-          <table>
+          {/* <table>
             <thead>
               <tr>
                 <th>Name</th>
@@ -59,7 +118,13 @@ function StudentTable(props) {
                 openModal={openModal} 
               />
             </tbody>
-          </table>
+          </table> */}
+           <Table
+            columns={columns}
+            dataSource={slicedStudentData} // sliced data for pagination
+            pagination={false} // Disable default ant design pagination
+            rowKey="id" // unique key for each row
+          />
           <Pagination
             currentPage={currentPage}
             totalPages={Math.ceil(userDetails.length / detailsPerPage)}
